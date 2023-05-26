b7a897413d146bc6880c4d46c0f75247c977dd66
Carolina Garzon <carogrzn97@gmail.com>
2023-05-26 06:15:54 -0300

    Changes to be committed: 	modified:   api/src/controllers/countriesControllers.js

--- api/src/controllers/countriesControllers.js
+++ api/src/controllers/countriesControllers.js
@@ -2,22 +2,37 @@ const { Country } = require('../db.js');
 const axios = require('axios')
 
 const getCountriesById = async (id, source) => {
-    const country = source === 'api' ?
-    await axios.get(`https://restcountries.com/v3/alpha/`)
-    : 'hola'
-}
-const foundCountry = async (name) => {
-    await axios.get(`https://restcountries.com/v3/name/${name}`)
+    const country = await (axios.get(`https://rest-countries.up.railway.app/v2/alpha/${id}`)).data;
     
+    await country.map((country) => {
+        let pais = {
+            id: country.alpha3Code,
+            name: country.name,
+            flagImg: country.flags[1],
+            continent: country.continents[0],
+            capital: country.capital ? country.capital : 'Capital not found',
+            subregion: country.subregion,
+            population: country.population,
+        }
+        return Country.findOrCreate({ pais })
+    })
 }
 
-const foundCountries = async () => {
-    const all = await axios.get('https://restcountries.com/v3/all');
+const foundCountry = async (name) => {
+    const countryWithName = await (axios.get(`https://rest-countries.up.railway.app/v2/name/${name}`)).data;
+    
+    return Country.findOrCreate({ where: {name: countryWithName.name}})
+
+}
+
+const searchCountries = async () => {
+    const all = await (axios.get('https://rest-countries.up.railway.app/v2/all')).data;
+    
 
 }
 
 module.exports = {
     getCountriesById,
     foundCountry, 
-    foundCountries
+    searchCountries
 }

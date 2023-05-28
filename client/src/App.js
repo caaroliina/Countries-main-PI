9dac826fe38aa854882007d68f7a3b35c789a9e1
Carolina Garzon <carogrzn97@gmail.com>
2023-05-27 21:43:50 -0300

    validation in back and start the front: 	new file:   api/src/middlewares/validate.js 	modified:   api/src/routes/activityRouter.js 	modified:   client/src/App.js 	new file:   client/src/components/Detail/Detail.jsx 	new file:   client/src/components/Form/Form.jsx 	new file:   client/src/components/Home/Home.jsx 	new file:   client/src/components/LandingPage/LandingPage.jsx 	new file:   client/src/images/airport.jpg 	new file:   client/src/images/landing1.png 	new file:   client/src/images/landingOp1.jpg 	new file:   client/src/images/passport.jpg 	modified:   client/src/index.js 	new file:   client/src/redux/action-type.js 	new file:   client/src/redux/action.js 	new file:   client/src/redux/reducer.js 	new file:   client/src/redux/store.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Form from './components/Form/Form';
+import Detail from './components/Detail/Detail';
+import LandingPage from './components/LandingPage/LandingPage';
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Countries</h1>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/detail/:id' element={<Detail/>}/>
+        <Route path='/form' element={<Form />}/>
+      </Routes>
     </div>
   );
 }
